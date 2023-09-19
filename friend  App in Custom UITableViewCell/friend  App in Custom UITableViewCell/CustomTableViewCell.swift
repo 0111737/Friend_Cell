06306b25007bae4703dcf68e226c07b374121366
Shimaa Alaa <104222951+0111737@users.noreply.github.com>
2023-09-19 13:29:21 -0700

    Add files via upload

---
+++ friend  App in Custom UITableViewCell/friend  App in Custom UITableViewCell/CustomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTableViewCell.swift
+//  friend  App in Custom UITableViewCell
+//
+//  Created by Shimaa Alaa Youssef on 09/09/2023.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ friend  App in Custom UITableViewCell/friend  App in Custom UITableViewCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  friend  App in Custom UITableViewCell
+//
+//  Created by Shimaa Alaa Youssef on 07/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
